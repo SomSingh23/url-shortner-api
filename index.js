becad6ffe3d40b7dad5ab469feb4e5141f8bc143
SomSingh23 <githubsom@gmail.com>
2023-08-06 22:43:18 +0530

    .env added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
 });
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to yarn");
+  res.status(200).send(`${process.env.secret}`);
 });
 app.get("/:id", (req, res) => {
   res.redirect("https://github.com/SomSingh23");
