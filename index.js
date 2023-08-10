d8ba436f4b556baa17973c363bd95a982a75758b
SomSingh23 <githubsom@gmail.com>
2023-08-10 20:22:45 +0530

    port changed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose
   });
 let app = express();
 app.use(cors());
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log(`Listening on port ${process.env.PORT}...`);
 });
 app.get("/", async (req, res) => {
