3456c94283750c0b7db552afd1003c75a7dc0f13
SomSingh23 <githubsom@gmail.com>
2023-08-12 13:38:52 +0530

    random string length changed to 10

--- random.js
+++ random.js
@@ -2,7 +2,7 @@ let randomstring = require("randomstring");
 let generateRandomString = () => {
   return randomstring.generate({
     charset: "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890",
-    length: 8,
+    length: 10,
   });
 };
 module.exports = generateRandomString;
