e991711adfb8e42e59d4b853b9f671ce24a36024
SomSingh23 <githubsom@gmail.com>
2023-08-21 22:37:32 +0530

    length changed from 9 to 10, .dockerignore changed

--- random.js
+++ random.js
@@ -2,7 +2,7 @@ let randomstring = require("randomstring");
 let generateRandomString = () => {
   return randomstring.generate({
     charset: "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890",
-    length: 9,
+    length: 10,
   });
 };
 module.exports = generateRandomString;
